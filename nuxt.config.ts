4573b9e67c50652a0efc45feb51540a18dc276f1
Timothy Alcaide <timothy.alcaide@gmail.com>
2025-08-14 11:12:01 +0200

    add shadcn ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/image']
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    'shadcn-nuxt',
+  ],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+    shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  },
 })
